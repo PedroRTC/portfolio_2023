b54a1a7c75f24244838d97f88bd13b93d31a0fce
Pedro Ronaldo <pedro06portfolio@gmail.com>
2023-04-09 14:24:35 -0300

    formação

--- js/services.js
+++ js/services.js
@@ -10,33 +10,31 @@ function showServices(element) {
     "container_all_services"
   );
   let all_services = createElementWithClass("section", "all_services");
-  let close_services=createElementWithClass("button","close_services")
+  let close_services = createElementWithClass("button", "close_services");
   let title_services = createElementWithClass("h4", "title_services");
   let list_services = createElementWithClass("div", "list_services");
 
-  close_services.textContent="X"
-
-  
+  close_services.textContent = "X";
 
   if (element.classList.value.includes("front")) {
     title_services.textContent = "Front end";
-    list_services.innerHTML=`<p>Portfólio</p><p>Cardapío virtual</p><p>Landing page</p><p>pagina de conteúdo</p>`
+    list_services.innerHTML = `<p>Portfólio</p><p>Cardapío virtual</p><p>Landing page</p><p>pagina de conteúdo</p>`;
   } else {
     title_services.textContent = "Design gráfico";
     list_services.innerHTML = `<p>Pánfleto</p><p>Convite</p><p>Logotípo</p><p>Cardapío</p><p>Cartão de visita</p><p>design de site</p>`;
   }
 
-  all_services.appendChild(close_services)
+  all_services.appendChild(close_services);
   all_services.appendChild(title_services);
   all_services.appendChild(list_services);
   container_all_services.appendChild(all_services);
   window.document.body.appendChild(container_all_services);
 
-  close_services.addEventListener("click",()=>removeServices(container_all_services))
+  close_services.addEventListener("click", () =>
+    removeServices(container_all_services)
+  );
 }
 
-
-
-function removeServices(container_all_services){
-    window.document.body.removeChild(container_all_services)
+function removeServices(container_all_services) {
+  window.document.body.removeChild(container_all_services);
 }
